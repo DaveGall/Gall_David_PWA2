3ced938acb7940c41b6b5a4d8bc32d26aa069946
David Gall <dlaker74@fullsail.edu>
2014-11-05 19:17:10 -0800

    Week 2 Project
    I have rearranged my design by taking out my three boxes in order to
    create my tabbed menu. I have also added a gallery and sidebar which
    will be completed shortly.

--- Gall_David_Project1/js/main.js
+++ Gall_David_Project1/js/main.js
@@ -5,14 +5,49 @@
 
 (function($){
 	
-	
+
 	/*
 	===============================================
 	========================= APPLICATION FUNCTIONS	
 	*/
-	
-	
-	var checkLoginState = function(){
+    $(".modalClick").on("click", function(event){
+       event.preventDefault();
+        $("#overlay")
+            .fadeIn()
+            .find("#modal")
+            .fadeIn();
+    });
+    $(".close").on("click", function(event){
+       event.preventDefault();
+        $("#overlay").fadeOut().find("#modal").fadeOut();
+    });
+
+    $(".myStatus").mouseover(function(){
+        $(this).fadeTo(100,.5);
+    });
+    $(".myStatus").mouseout(function(){
+       $(this).fadeTo(100, 1);
+    });
+
+    $("tbody tr:even").addClass("even");
+	$("tbody tr:odd").addClass("odd");
+
+    $(".masterTooltip").hover(function(){
+        var title = $(this).attr("title");
+        $(this).data("tipText", title).removeAttr("title");
+        $('<p class="tooltip"></p>').text(title).appendTo("body").fadeIn("fast");
+    }, function(){
+        $(this).attr("title", $(this).data("tipText"));
+        $(".tooltip").remove();
+    }).mousemove(function(e){
+        var mouseX = e.pageX + 20;
+        var mouseY = e.pageY + 10;
+        $(".tooltip").css({top: mouseY, left: mouseX})
+    });
+
+
+
+    var checkLoginState = function(){
 		$.ajax({
 			url: 'xhr/check_login.php',
 			type: 'get',
@@ -23,6 +58,8 @@
 			}
 		});
 	};
+
+
 	
 	
 
@@ -48,10 +85,10 @@
 	==================================== END EVENTS 
 	===============================================
 	*/
-		
-		
 
-	
+
+
+
 })(jQuery); // end private scope
 
 
